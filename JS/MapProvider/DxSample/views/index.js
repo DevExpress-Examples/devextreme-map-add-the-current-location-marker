f14af24e01bfa607869f1d36cb44679cd93ec5f0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ JS/MapProvider/DxSample/views/index.js
@@ -0,0 +1,31 @@
+﻿DxSample.Index = function (params) {
+    var viewModel = {
+        lat: ko.observable(40.749825),
+        lng: ko.observable(-73.987963),
+
+        mapReadyAction: function (s) {
+            var map = s.component;
+            if (navigator.geolocation) {
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function (position) {
+
+                        viewModel.lat(position.coords.latitude);
+                        viewModel.lng(position.coords.longitude);
+
+                        map.addMarker({
+                            location: [viewModel.lat(), viewModel.lng()],
+                            tooltip: 'You\'re here'
+                        });
+
+                    }, function () {
+                        handleNoGeolocation(true);
+                    });
+                } else {
+                    // Browser doesn't support Geolocation
+                    handleNoGeolocation(false);
+                };
+            }
+        }
+    };
+    return viewModel;
+}
